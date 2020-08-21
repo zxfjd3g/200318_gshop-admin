51ceae87ef45e2c8d70462026b78864e83eca8bb
zxfjd3g <258147149@qq.com>
2020-08-21 09:38:23 +0800

    attrList

--- src/settings.js
+++ src/settings.js
@@ -6,7 +6,7 @@ module.exports = {
   /* 
   主界面左侧Logo标题
   */
-  title: '谷粒商城后台',
+  title: 'admin',
 
   /* 
   是否固定主界面的Header部分
--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -3,7 +3,29 @@
     <el-card style="margin-bottom:20px">
       <category-selector @categoryChange="handleCategoryChange"></category-selector>
     </el-card>
-    <el-card>BBB</el-card>
+    <el-card>
+      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+      <el-table
+        v-loading="loading"
+        style="margin-top: 20px"
+        border
+        :data="attrs"
+      >
+        <el-table-column type="index" label="序号" width="80" align="center"/>
+        <el-table-column label="属性名称" width="150" prop="attrName"/>
+        <el-table-column label="属性值列表">
+          <template v-slot="{row, $index}">
+            <el-tag v-for="(value, index) in row.attrValueList" :key="value.id" >{{value.valueName}}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template v-slot="{row, $index}">
+            <hint-button title="修改属性" type="warning" size="small" icon="el-icon-edit"/>
+            <hint-button title="删除属性" type="danger" size="small" icon="el-icon-delete"/>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
@@ -17,6 +39,7 @@ export default {
       category2Id: '', // 二级分类id
       category3Id: '', // 三级分类id
       attrs: [], // 属性列表
+      loading: false,
     }
   },
 
@@ -46,10 +69,11 @@ export default {
     */
     async getAttrs () {
       const { category1Id, category2Id, category3Id } = this
+      this.loading = true
       const result = await this.$API.attr.getAttrList(category1Id, category2Id, category3Id)
+      this.loading = false
       const attrs = result.data
       this.attrs = attrs
-      console.log('----', attrs)
     }
   }
 }
