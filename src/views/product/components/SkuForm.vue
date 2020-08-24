fe5e5ed856c73cf26fab299554a9a3abf6476da2
zxfjd3g <258147149@qq.com>
2020-08-24 15:46:40 +0800

    skuForm收集数据

--- src/views/product/components/SkuForm.vue
+++ src/views/product/components/SkuForm.vue
@@ -1,43 +1,30 @@
 <template>
   <el-form label-width="100px">
     <el-form-item label="SPU 名称">
-      <span>AAA</span>
+      <span>{{spu.spuName}}</span>
     </el-form-item>
 
     <el-form-item label="SKU 名称">
-      <el-input type="text" placeholder="SKU 名称"></el-input>
+      <el-input type="text" placeholder="SKU 名称" v-model="skuInfo.skuName"></el-input>
     </el-form-item>
     
     <el-form-item label="价格(元)">
-      <el-input type="number" placeholder="SKU 价格"></el-input>
+      <el-input type="number" placeholder="SKU 价格" v-model="skuInfo.price"></el-input>
     </el-form-item>
 
     <el-form-item label="重量(千克)">
-      <el-input type="number" placeholder="SKU 重量"></el-input>
+      <el-input type="number" placeholder="SKU 重量" v-model="skuInfo.weight"></el-input>
     </el-form-item>
 
     <el-form-item label="规格描述">
-      <el-input type="textarea" placeholder="SKU 规格描述" rows="4"></el-input>
+      <el-input type="textarea" placeholder="SKU 规格描述" rows="4" v-model="skuInfo.skuDesc"></el-input>
     </el-form-item>
 
     <el-form-item label="平台属性">
       <el-form inline label-width="100px">
-        <el-form-item label="属性一" style="margin: 5px 0">
-          <el-select placeholder="请输入" value="">
-            <el-option label="AA" :value="1"></el-option>
-            <el-option label="BB" :value="2"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="属性二" style="margin: 5px 0">
-          <el-select placeholder="请输入" value="">
-            <el-option label="AA" :value="1"></el-option>
-            <el-option label="BB" :value="2"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="属性三" style="margin: 5px 0">
-          <el-select placeholder="请输入" value="">
-            <el-option label="AA" :value="1"></el-option>
-            <el-option label="BB" :value="2"></el-option>
+        <el-form-item :label="attr.attrName" style="margin: 5px 0" v-for="attr in attrList">
+          <el-select placeholder="请输入" v-model="attr.attrIdValueId">
+            <el-option :label="value.valueName" :value="attr.id + ':' + value.id" v-for="value in attr.attrValueList" :key="value.id"></el-option>
           </el-select>
         </el-form-item>
       </el-form>
@@ -45,36 +32,33 @@
 
     <el-form-item label="销售属性">
       <el-form inline label-width="100px">
-        <el-form-item label="属性一" style="margin: 5px 0">
-          <el-select placeholder="请输入" value="">
-            <el-option label="AA" :value="1"></el-option>
-            <el-option label="BB" :value="2"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="属性二" style="margin: 5px 0">
-          <el-select placeholder="请输入" value="">
-            <el-option label="AA" :value="1"></el-option>
-            <el-option label="BB" :value="2"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="属性三" style="margin: 5px 0">
-          <el-select placeholder="请输入" value="">
-            <el-option label="AA" :value="1"></el-option>
-            <el-option label="BB" :value="2"></el-option>
+        <el-form-item :label="attr.saleAttrName" style="margin: 5px 0" v-for="attr in spuSaleAttrList" :key="attr.id">
+          <el-select placeholder="请输入" v-model="attr.saleAttrValueId">
+            <el-option :label="value.saleAttrValueName" :value="value.id" v-for="value in attr.spuSaleAttrValueList" :key="value.id"></el-option>
           </el-select>
         </el-form-item>
       </el-form>
     </el-form-item>
 
     <el-form-item label="图片列表">
-      <el-table border>
+      <el-table border :data="spuImageList"  @selection-change="handleSelectionChange">
+        <!-- 可以多选的 -->
         <el-table-column
           type="selection"
           width="55">
         </el-table-column>
-        <el-table-column label="图片"></el-table-column>
+        <el-table-column label="图片">
+          <template v-slot="{row}">
+            <img :src="row.imgUrl" alt="" style="width:100px; height:100px;">
+          </template>
+        </el-table-column>
         <el-table-column label="名称" prop="imgName"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="操作">
+          <template v-slot="{row}">
+            <el-tag type="success" v-if="row.isDefault==='1'">默认</el-tag>
+            <el-button type="primary" size="small" v-else @click="setDefault(row)">设为默认</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-form-item>
 
@@ -112,10 +96,35 @@ export default {
       attrList: [], // 平台属性列表
       spuSaleAttrList: [], // spu销售属性列表
       spuImageList: [], // spu图片列表
+      // selectedSpuImageList: []
     }
   },
 
   methods: {
+
+    /* 
+    val: 所有选中的行数据对象的数组
+    */
+    handleSelectionChange(val) {
+      console.log(val)
+      // this.selectedSpuImageList = val;
+      // 保存选中的图片列表
+      this.skuInfo.skuImageList = val
+    },
+
+    /* 
+    设置默认图片
+    */
+    setDefault (spuImage) {
+      // 将原本默认的变为非默认(都指定为非默认的)
+      this.spuImageList.forEach(image => image.isDefault = '0')
+      // 将当前变为默认的
+      spuImage.isDefault = '1'
+
+      // 保存当前图片url为sku默认的图片url
+      this.skuInfo.skuDefaultImg = spuImage.imgUrl
+    },
+
     /* 
     请求加载添加界面初始显示需要的数据
     */
@@ -142,7 +151,7 @@ export default {
       // 只有都成功了, 才显示
       const results = await Promise.all([p1, p2, p3])   // [result1, result2, result3]
       this.attrList = results[0].data
-      this.spuImageList = results[1].data
+      this.spuImageList = results[1].data.map(item => ({...item, isDefault: '0'}))
       this.spuSaleAttrList = results[2].data
     }
   }
